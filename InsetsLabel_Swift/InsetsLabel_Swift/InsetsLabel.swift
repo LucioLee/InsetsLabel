324774c4880023ea0d1006ce96e8795186b81385
LucioLee <990467229@qq.com>
2016-10-19 16:19:05 +0800

    commit swift3 version code

---
+++ InsetsLabel_Swift/InsetsLabel_Swift/InsetsLabel.swift
@@ -0,0 +1,77 @@
+//
+//  InsetsLabel.swift
+//  InsetsLabel
+//
+//  Created by LucioLee on 16/3/9.
+//
+//
+
+import UIKit
+
+@IBDesignable open class InsetsLabel: UILabel {
+    
+    open var insets = UIEdgeInsets.zero
+    
+    @IBInspectable open var topInset: CGFloat {
+        set {
+            insets.top = newValue
+        }
+        get {
+            return insets.top
+        }
+    }
+    
+    @IBInspectable open var leftInset: CGFloat {
+        set {
+            insets.left = newValue
+        }
+        get {
+            return insets.left
+        }
+    }
+    
+    @IBInspectable open var bottomInset: CGFloat {
+        set {
+            insets.bottom = newValue
+        }
+        get {
+            return insets.bottom
+        }
+    }
+    
+    @IBInspectable open var rightInset: CGFloat {
+        set {
+            insets.right = newValue
+        }
+        get {
+            return insets.right
+        }
+    }
+    
+    override open var intrinsicContentSize: CGSize {
+        get {
+            var size = super.intrinsicContentSize
+            size.width  += insets.left + insets.right
+            size.height += insets.top + insets.bottom
+            return size
+        }
+    }
+    
+    convenience public init(insets: UIEdgeInsets) {
+        self.init()
+        self.insets = insets;
+    }
+    
+    convenience public init(frame: CGRect, insets: UIEdgeInsets) {
+        self.init(frame:frame)
+        self.insets = insets
+    }
+    
+    override open func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        return super.textRect(forBounds: UIEdgeInsetsInsetRect(bounds, insets), limitedToNumberOfLines: numberOfLines)
+    }
+    
+    override open func drawText(in rect: CGRect) {
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+    }
+}
---
+++ InsetsLabel_Swift/InsetsLabel_Swift/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  InsetsLabel_Swift
+//
+//  Created by 李新新 on 2016/10/19.
+//  Copyright © 2016年 LucioLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyBoardLabel: InsetsLabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let codeLabel = InsetsLabel(insets: UIEdgeInsets(top: 10, left: 20, bottom: 20, right: 20))
+        codeLabel.translatesAutoresizingMaskIntoConstraints = false
+        codeLabel.numberOfLines = 0
+        codeLabel.lineBreakMode = NSLineBreakMode.byCharWrapping
+        codeLabel.backgroundColor = UIColor.orange
+        codeLabel.text = "\"Communist Party of China Constitution\" said: The Chinese Communist Party is the vanguard of the working class, while the Chinese people and the vanguard of the Chinese nation is the cause of socialism with Chinese characteristics, the core of leadership, on behalf of the development requirements of China's advanced productive forces, on behalf of China's advanced culture the way forward, on behalf of the Chinese people's fundamental interests. The party's highest ideal and ultimate goal is to realize communism. Communist Party of China takes Marxism-Leninism, Mao Zedong Thought, Deng Xiaoping Theory and \"Three Represents\" as its guide to action. Communist Party of China in the primary stage of socialism's basic line is: to lead and unite people of all nationalities to economic construction, the four cardinal principles of reform and opening, self-reliance, hard work and strive to build China into a prosperous, democratic, civilized and harmonious modern socialist country of the party."
+        view.addSubview(codeLabel)
+        
+        let constraintH = NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[view]-0-|",
+                                                         options: NSLayoutFormatOptions(),
+                                                         metrics: nil,
+                                                         views: ["view":codeLabel])
+        let constraintV = NSLayoutConstraint.constraints(withVisualFormat: "V:[storyBoardLabel]-[codeLabel]",
+                                                         options: NSLayoutFormatOptions(),
+                                                         metrics: nil,
+                                                         views: ["codeLabel":codeLabel,"storyBoardLabel":storyBoardLabel])
+        
+        view.addConstraints(constraintH + constraintV)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
